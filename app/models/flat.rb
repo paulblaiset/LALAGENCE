dd5e63f05b9ddb96e432a3f89e20eb707228b671
Baptiste Daniellou <b.daniellou@orange.fr>
2018-06-01 09:16:39 -0400

    photo uploader ok

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -2,4 +2,6 @@ class Flat < ApplicationRecord
   belongs_to :agency, optional: true
   has_many :url_flats, dependent: :destroy
   has_many :candidatures, through: :url_flats
+
+  mount_uploader :photo, PhotoUploader
 end
