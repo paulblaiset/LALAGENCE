e8ef3ce8e623985dcb5c7d7aa500cfe0361420ba
Paul Henri Blaiset <paul.blaiset@gmail.com>
2018-06-01 11:33:22 +0200

    correction bug affichage 0 guarantor form

--- app/models/guarantor.rb
+++ app/models/guarantor.rb
@@ -8,6 +8,7 @@ class Guarantor < ApplicationRecord
   mount_uploader :pay_slip_3, PhotoUploader
   mount_uploader :tax_notice, PhotoUploader
   mount_uploader :property_tax, PhotoUploader
+  mount_uploader :actual_rent_bill, PhotoUploader
 
   def has_required_field?
     self.gender && self.first_name && self.last_name && self.birthday_date && self.address && self.nationality && self.couple_status && self.mail && self.child_number && self.job_name && self.employer && self.employer_address && self.employer_phone_number && self.seniority && self.year_legal_income && self.actual_rent_price && self.employment_contract? && self.identity_card? && self.property_tax? && self.pay_slip_1? && self.pay_slip_2? && self.pay_slip_3? && self.rental_status && self.tax_notice?
