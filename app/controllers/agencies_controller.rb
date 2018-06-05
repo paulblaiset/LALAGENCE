5dc82bb3c7dc070a91f2caac4e1de5a8d62f1ff3
Ulrick Labeau <ulrick.labeau@gmail.com>
2018-06-05 15:33:07 +0200

    btn download

--- app/controllers/agencies_controller.rb
+++ app/controllers/agencies_controller.rb
@@ -1,6 +1,7 @@
-require 'rubygems'
+require 'zip'
 
 class AgenciesController < ApplicationController
+  skip_after_action :verify_authorized, only: [:download]
 
   def show
     @agency = Agency.find(params[:id])
@@ -11,24 +12,24 @@ class AgenciesController < ApplicationController
   end
 
   def download
-    input_filenames = []
     @user = User.find(params[:id])
     user_files = [@user.employment_contract, @user.identity_card, @user.pay_slip_1, @user.pay_slip_2, @user.pay_slip_3, @user.actual_rent_bill, @user.tax_notice]
-    user_files.each do |doc|
-      file = File.open("/tmp/docment-#{rand(100)}.jpg", "w") { |f| IO.copy_stream(open(doc.url), f) }
-      input_filenames << file.path
-    end
-    folder = Rails.root
-    zipfile_name = "/tmp/candidature.zip"
-    Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
-      input_filenames.each do |filename|
-      zipfile.add(filename, File.join(folder, filename))
+    filename = 'candidature.zip'
+    temp_file = Tempfile.new(filename)
+    Zip::OutputStream.open(temp_file) { |zos| }
+    Zip::File.open(temp_file.path, Zip::File::CREATE) do |zip|
+      user_files.each do |doc|
+        path = "#{Rails.root}/tmp/#{doc.mounted_as}"
+        File.open(path, "w") { |f| IO.copy_stream(open(doc.url), f) }
+        zip.add(doc.mounted_as, path)
       end
     end
-    send_file(
-      "#{Rails.root}/#{zipfile_name}",
+    send_data(
+      File.read(temp_file.path),
       filename: "candidatures.zip",
       type: "application/zip"
     )
+    temp_file.close
+    temp_file.unlink
   end
 end
