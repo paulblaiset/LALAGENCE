bda57e044794960a4580488ee4ec6a3ba59c90ce
Ulrick Labeau <ulrick.labeau@gmail.com>
2018-06-06 12:30:22 +0200

    ajout photo profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_05_153955) do
+ActiveRecord::Schema.define(version: 2018_06_06_092826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -130,6 +130,7 @@ ActiveRecord::Schema.define(version: 2018_06_05_153955) do
     t.string "tax_notice"
     t.bigint "agency_id"
     t.string "role"
+    t.string "photo"
     t.index ["agency_id"], name: "index_users_on_agency_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
